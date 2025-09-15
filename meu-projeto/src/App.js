ea26588ae83569bf8b3885a40f7d729529557426
thais-quirino <thaisuckeman@yahoo.com.br>
2025-09-15 13:48:31 -0300

    feature: reitulizando e craindo  components

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWord from './components/HelloWorld';
 
 function App() {
   const name = 'Thais';
@@ -17,6 +18,7 @@ function App() {
       <p>Olá,{newName} </p>
       <p>Soma:{sum(1, 2)}</p>
       <img src={url} alt="Minha Imagem" />
+      <HelloWord />
     </div>
   );
 }
