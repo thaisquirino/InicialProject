4457b81a687c3ad59076c94c141af6f1b39472aa
thais-quirino <thaisuckeman@yahoo.com.br>
2025-09-15 12:48:25 -0300

    feature:modelos de Jsx

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const name = 'Thais';
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://picsum.photos/150';
+
   return (
     <div className="App">
       <h1>Olá Mundo!</h1>
       <p>Meu Primeiro App</p>
+      <p>Olá,{newName} </p>
+      <p>Soma:{sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
